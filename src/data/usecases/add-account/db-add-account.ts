5143d296c6d66d8412f71784bfe077990a3b1d09
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-02-14 09:39:24 -0300

    Integração com o addAccountRepository

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,21 @@
-import { AccountModel } from '../../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { Encrypter } from '../../protocols/encrypter'
+
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    // Object.assign = Permite alterar apenas uma propriedade do objeto original
+    // Basicamente, estamos criando um novo objeto com base no accountData, mas com o password diferente do original
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+
     return new Promise(resolve => resolve(null))
   }
 }
