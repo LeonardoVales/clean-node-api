860a07ac493479c30c1bcf3d8de4bec9e89e2785
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-02-15 08:15:15 -0300

    Testes do bcrypt

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,13 +1,42 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
 describe('Bcrypt Adapter', () => {
-  it('Should call bcrypt with corret value', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+  it('Should call bcrypt with corret values', async () => {
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenLastCalledWith('any_value', salt)
+  })
+
+  it('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hash')
+  })
+
+  it('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
   })
 })
