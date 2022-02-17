6eac6dff26633acbd9c71ca6f9334f8ada288f79
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-02-17 16:23:40 -0300

    Implementa middleware para parsear o json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,5 @@
 // Aqui é onde cria as instâncias de todas as outras camadas
 // Aqui é onde fica a árvore de dependência dos controladores e os factorys
+import app from './config/app'
 
-import express from 'express'
-
-const app = express()
 app.listen(5050, () => console.log('server running at http:localhost:5050'))
