3b8fbb9b5e806b96ed86aa21b3c75bc65ead9ff6
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-03-21 11:25:42 -0300

    Isola a validação de senha e confirmação de senha no validationComposite

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -83,35 +83,11 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Signup controller', () => {
-  it('Should return 400 if password confirmation fails', async () => {
-    // system under test
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_namel',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'invalid_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
-  })
-
   it('Should return 400 if an invalid email is provided', async () => {
     // system under test
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
 
-    // const httpRequest = {
-    //   body: {
-    //     name: 'any_name',
-    //     email: 'invalid_email@mail.com',
-    //     password: 'any_password',
-    //     passwordConfirmation: 'any_password'
-    //   }
-    // }
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
