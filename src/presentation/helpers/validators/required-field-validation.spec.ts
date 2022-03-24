a2d27384e16b9532a9cedc74c372a825c5eb727a
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-03-24 10:11:21 -0300

    Testes do compareFiels e validationFields

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,22 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
+describe('RequiredField Validation', () => {
+  it('Should return a MissingParamerror if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_name' })
+
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  it('Should not return if validation success', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_name' })
+
+    expect(error).toBeFalsy()
+  })
+})
