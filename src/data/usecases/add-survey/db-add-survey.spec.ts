897869aabc54d26698ab93dd84ab9d175ba5f715
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-05-25 09:31:09 -0300

    Cria teste de retorno de erro

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -43,4 +43,14 @@ describe('DbAddSurvey UseCase', () => {
 
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+
+  it('Should throw if hasher throws', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+    const promise = sut.add(makeFakeSurveyData())
+
+    await expect(promise).rejects.toThrow()
+  })
 })
