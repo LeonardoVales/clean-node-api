bd8af95eeea480e66640a2c9ad3c4679b04820e3
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-06-23 13:52:24 -0300

    Integração do loadSurveyController com interface do caso de uso

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -0,0 +1,67 @@
+import { LoadSurveysController } from './load-surveys-controller'
+import { SurveyModel, LoadSurveys } from './load-survey-controller-protocols'
+import MockDate from 'mockdate'
+
+interface SutTypes {
+  sut: LoadSurveysController
+  loadSurveyStub: LoadSurveys
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyStub = makeLoadSurveys()
+  const sut = new LoadSurveysController(loadSurveyStub)
+
+  return {
+    sut,
+    loadSurveyStub
+  }
+}
+
+const makeLoadSurveys = (): LoadSurveys => {
+  class LoadSurveysStub implements LoadSurveys {
+    async load (): Promise<SurveyModel[]> {
+      return new Promise(resolve => resolve(makeFakeSurveys()))
+    }
+  }
+
+  return new LoadSurveysStub()
+}
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [{
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  },
+  {
+    id: 'other_id',
+    question: 'other_question',
+    answers: [{
+      image: 'other_image',
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  }]
+}
+
+describe('LoadSurveysController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('Should call LoadSurveys', async (): Promise<void> => {
+    const { sut, loadSurveyStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveyStub, 'load')
+    await sut.handle({})
+
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})
