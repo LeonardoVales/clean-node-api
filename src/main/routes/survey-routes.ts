c21bd6907a07df065e4922c8c40cf185ad2a5404
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-06-27 17:23:58 -0300

    Implementa rota para consultar enquentes e valida se o token foi passado

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,10 +3,15 @@ import { makeAddSurveyController } from '../factories/controllers/survey/add-sur
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { adaptMiddlware } from '../adapters/express-middleware-adapter'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddlware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddlware(makeAuthMiddleware())
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
