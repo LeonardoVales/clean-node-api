ddf1a96ebd4a5f978bff4c13e0fd6cc402079177
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-06-27 09:55:25 -0300

    Implementa loadAll no SurveyMongoRepository

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -3,8 +3,17 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'bson'
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys && MongoHelper.mappingAll(surveys)
+  }
+
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     const { insertedId: id } = await surveyCollection.insertOne(surveyData)
