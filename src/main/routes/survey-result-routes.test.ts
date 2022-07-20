d7e62eb1b53f4a2c1c0d02b3b6921f896d62b9e4
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-07-20 15:39:40 -0300

    Implementa composição para a rota do save survey result

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,80 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { ObjectId } from 'bson'
+import { sign } from 'jsonwebtoken'
+import env from '@/main/config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Leonardo',
+    email: 'leo@gmail.com',
+    password: 'any_password'
+  })
+  const { insertedId: id } = res
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    it('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+
+    it('Should return 200 on save survey result without accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const { insertedId } = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+
+      await request(app)
+        .put(`/api/surveys/${insertedId.toString()}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200)
+    })
+  })
+})
---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { auth } from '@/main/middlewares/auth'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+
+export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}
