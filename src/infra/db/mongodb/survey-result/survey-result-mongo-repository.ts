14a2f4ef38a2e4f9f192e5c3c921bd5342a92b2b
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-08-09 08:45:19 -0300

    Corrige bug no survey-result-mongo-repository

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -203,6 +203,6 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository, 
 
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
 
-    return surveyResult?.length ? surveyResult[0] as SurveyResultModel : null
+    return surveyResult.length ? surveyResult[0] as SurveyResultModel : null
   }
 }
